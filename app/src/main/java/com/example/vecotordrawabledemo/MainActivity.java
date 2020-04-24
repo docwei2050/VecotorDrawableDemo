163e8b13c8a324aa64f1af3c096dcf6e4d3b2cf2
docwei <doc_wei@163.com>
2020-04-24 17:19:35 +0800

    home图使用path动画 其他tab使用透明度动画

--- app/src/main/java/com/example/vecotordrawabledemo/MainActivity.java
+++ app/src/main/java/com/example/vecotordrawabledemo/MainActivity.java
@@ -15,28 +15,18 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView mImageView1;
     private ImageView mImageView2;
+    private ImageView mImageView3;
+    private ImageView mImageView4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mImageView1 = (ImageView) findViewById(R.id.imageView1);
-        mImageView2 = findViewById(R.id.imageView2);
-  /*      mImageView2 = (ImageView) findViewById(R.id.imageView2);
+        mImageView1 = (ImageView) findViewById(R.id.imageView);
+        mImageView2 = (ImageView) findViewById(R.id.imageView2);
         mImageView3 = (ImageView) findViewById(R.id.imageView3);
         mImageView4 = (ImageView) findViewById(R.id.imageView4);
-        AnimatedVectorDrawableCompat animatedVectorDrawableCompat1 = AnimatedVectorDrawableCompat.create(
-                this, R.drawable.animted_vector);
-        AnimatedVectorDrawableCompat animatedVectorDrawableCompat2 = AnimatedVectorDrawableCompat.create(
-                this, R.drawable.path2_anim);
-        AnimatedVectorDrawableCompat animatedVectorDrawableCompat3 = AnimatedVectorDrawableCompat.create(
-                this, R.drawable.path3_anim);
-        AnimatedVectorDrawableCompat animatedVectorDrawableCompat4 = AnimatedVectorDrawableCompat.create(
-                this, R.drawable.path4_anim);
-        mImageView1.setImageDrawable(animatedVectorDrawableCompat1);
-        mImageView2.setImageDrawable(animatedVectorDrawableCompat2);
-        mImageView3.setImageDrawable(animatedVectorDrawableCompat3);
-        mImageView4.setImageDrawable(animatedVectorDrawableCompat4);*/
+
 
         mImageView1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,36 +38,48 @@ public class MainActivity extends AppCompatActivity {
                     Animatable animatable = (Animatable) current;
                    animatable.start();
                 }
-                mImageView2.setSelected(true);
-                StateListDrawable drawable2 = (StateListDrawable) mImageView2.getDrawable();
-                Drawable current2 =drawable2.getCurrent();
-                if (current2 instanceof Animatable) {
-                    Animatable animatable = (Animatable) current2;
-                    animatable.start();
-                }
+
             }
         });
 
-       /* mImageView2.setOnClickListener(new View.OnClickListener() {
+        mImageView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((Animatable)mImageView2.getDrawable()).start();
+                mImageView2.setSelected(true);
+                StateListDrawable drawable = (StateListDrawable) mImageView2.getDrawable();
+                Drawable current =drawable.getCurrent();
+                if (current instanceof Animatable) {
+                    Animatable animatable = (Animatable) current;
+                    animatable.start();
+                }
             }
         });
 
         mImageView3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((Animatable)mImageView3.getDrawable()).start();
+                mImageView3.setSelected(true);
+                StateListDrawable drawable = (StateListDrawable) mImageView3.getDrawable();
+                Drawable current =drawable.getCurrent();
+                if (current instanceof Animatable) {
+                    Animatable animatable = (Animatable) current;
+                    animatable.start();
+                }
             }
         });
 
         mImageView4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((Animatable)mImageView4.getDrawable()).start();
+                mImageView4.setSelected(true);
+                StateListDrawable drawable = (StateListDrawable) mImageView4.getDrawable();
+                Drawable current =drawable.getCurrent();
+                if (current instanceof Animatable) {
+                    Animatable animatable = (Animatable) current;
+                    animatable.start();
+                }
             }
-        });*/
+        });
 
 
     }
